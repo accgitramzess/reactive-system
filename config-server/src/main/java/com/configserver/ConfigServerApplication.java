fada127ca8d70df88672a6b41840722f7b6514f3
roman kuzmin <javaworkdeposit@gmail.com>
2018-10-12 18:26:45 +0300

    added config server and eureka server fixes

--- config-server/src/main/java/com/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/configserver/ConfigServerApplication.java
@@ -2,8 +2,11 @@ package com.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {GsonAutoConfiguration.class})
 @EnableConfigServer
 @EnableEurekaClient
 public class ConfigServerApplication {
--- event-collector/src/main/java/event/protocol/HttpProtocolProvider.java
+++ event-collector/src/main/java/event/protocol/HttpProtocolProvider.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
 
 import commons.dao.Event;
@@ -18,11 +19,12 @@ public class HttpProtocolProvider implements ProtocolProvider<Event> {
 
     @Override
     public List<Event> fetchEvents() {
-        return restTemplate.exchange(
+        /*return restTemplate.exchange(
                 baseUrl + uri,
                 HttpMethod.GET,
                 new HttpEntity<>(headers),
                 new ParameterizedTypeReference<List<T>>() {}, variables
-        );
+        );*/
+        return Collections.emptyList();
     }
 }
